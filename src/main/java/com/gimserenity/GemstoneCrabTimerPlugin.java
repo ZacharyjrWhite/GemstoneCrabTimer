ac05939f7b1c206101b2963c4b86a781c17d0434
Shane Caulkett <shane_is_shane123@hotmail.co.uk>
2025-08-05 23:52:26 +0100

    change notifier to use config value in message

--- src/main/java/com/gimserenity/GemstoneCrabTimerPlugin.java
+++ src/main/java/com/gimserenity/GemstoneCrabTimerPlugin.java
@@ -767,7 +767,7 @@ public class GemstoneCrabTimerPlugin extends Plugin
 			if (hpPercent <= (config.hpThreshold()) && !notificationSent)
 			{
 				notificationSent = true;
-				notifier.notify(config.hpThresholdNotification(), config.notificationMessage() + " (" + hpPercent + "% HP)");
+				notifier.notify(config.hpThresholdNotification(), config.notificationMessage() + " (" + config.hpThreshold() + "% HP)");
 				log.debug("Sent notification for Gemstone Crab at {}% HP", hpPercent);
 			}
 		}
